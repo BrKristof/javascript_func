ea958f4ea3b0695d8d685e3bd78732e1fc1f0aa6
Brandhuber Kristof <brandhuber.kristof@BOLYAI.LOCAL>
2025-10-10 08:56:26 +0200

    2025.10.10

---
+++ table_row/script.js
@@ -0,0 +1,31 @@
+
+const arr = {}
+
+let num  = 7
+
+if(num > 5){
+
+    arr.Name = "szoveg"
+}
+else{
+    
+    arr.Name = "else szove"
+}
+
+console.log(arr.Name)
+
+
+console.log(arr['Name'])
+
+if(arr.age == undefined){
+
+    
+}
+if(arr.age == null) //igaz
+{
+
+}
+if(arr.age === null) //hamis, csak akkor használjuk ha null értéket akarunk mindenkép
+{
+
+}
