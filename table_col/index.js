c340ba996f9254d7c4703b38ab6aaf8025aab8e8
Brandhuber Kristof <brandhuber.kristof@BOLYAI.LOCAL>
2025-10-17 10:06:33 +0200

    func

--- table_col/index.js
+++ table_col/index.js
@@ -83,3 +83,17 @@ for (const a of arr){
 
 
 }
+
+/**
+ * @param {string} celltype 
+ * @param {string} cellcontent 
+ * @param {HTMLTableRowElement} cellrow
+ */
+function CreateCellElement(celltype,cellcontent,cellrow){
+
+    const cell = document.createElement(celltype)
+    cell.innerText = cellcontent
+    cellrow.appendChild(cell)
+
+
+}
