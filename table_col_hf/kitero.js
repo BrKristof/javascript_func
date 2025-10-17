2da11d75f2d6480d749071b94d47d0fb8af6c070
Brandhuber Kristof <brandhuber.kristof@BOLYAI.LOCAL>
2025-10-17 09:38:37 +0200

    függvény alapok

---
+++ table_col_hf/kitero.js
@@ -0,0 +1,41 @@
+
+let i = 6
+
+/**
+ * @returns {void}
+ */
+function Vmi(){
+
+    console.log("nemtom")
+}
+Vmi()
+
+function Vmi2(){
+
+    console.log(i)
+}
+Vmi2()
+
+function Vmi3(){
+
+    const b = 7
+    console.log(b)
+}
+Vmi3()
+
+/**
+ * @param {number} param1 
+ * @returns {number}
+ */
+function Vmi4(param1){
+
+    const num = 6
+    const sum = num + param1
+    
+
+    return sum
+
+}
+
+const idk = Vmi4(2)
+console.log(idk)
