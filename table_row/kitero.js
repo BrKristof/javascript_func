3567a3e5c993fe6e274a44c3733296a6513d18db
Brandhuber Kristof <brandhuber.kristof@BOLYAI.LOCAL>
2025-11-07 10:05:52 +0100

    orai 11.07

--- table_row/kitero.js
+++ table_row/kitero.js
@@ -1,5 +1,7 @@
 
-
+/**
+ * @type {{from:string ,person:string ,piece:string ,piece2:?string ,person2:?string}[]}
+ */
 const arr = [
     {
         from:'Orosz',
@@ -96,3 +98,152 @@ for (const a of arr){
 
 
 }
+
+const form = document.getElementById('htmlform')
+form.addEventListener('submit', function(e){
+
+    e.preventDefault()
+    /**
+     * @type {HTMLFormElement}
+     */
+    const form = e.target
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const n = form.querySelector('#nemzetiseg')
+    /**
+     * @type {string}
+     */
+    const nvalue = n.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const a = form.querySelector('#mu1')
+        /**
+     * @type {string}
+     */
+    const avalue = a.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const p = form.querySelector('#nemzetiseg')
+        /**
+     * @type {string}
+     */
+    const pvalue = p.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const a2 = form.querySelector('#szerzo2')
+        /**
+     * @type {string}
+     */
+    const a2value = a2.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const p2= form.querySelector('#mu2')
+        /**
+     * @type {string}
+     */
+    const p2value = p2.value
+
+
+    /**
+     * @type {{from:string ,person:string ,piece:string ,piece2:?string ,person2:?string}}
+     */
+    const obj  = {}
+    obj.from  = nvalue
+    obj.a  = avalue
+    obj.p = pvalue
+    obj.a2 = a2value
+    obj.p2 = p2value
+
+    const tbody = document.getElementById('body')
+
+    const tr = document.createElement('tr')
+    tbody.appendChild(tr)
+
+    const td = document.createElement('td')
+    td.innerText = nvalue
+    tr.appendChild(td)
+
+    const td2 = document.createElement('td')
+    td2.innerText = avalue
+    tr.appendChild(td2)
+
+    const td3 = document.createElement('td')
+    td3.innerText = pvalue
+    tr.appendChild(td3)
+
+    if(a2value != '' && p2value != ''){
+
+        const ptr = document.createElement('tr')
+        tbody.appendChild(ptr)
+
+        const td4 = document.createElement('td')
+        td4.innerText = a2value
+        ptr.appendChild(td4)
+
+        const td5 = document.createElement('td')
+        td5.innerText = p2value
+        ptr.appendChild(td5)
+
+        td.rowSpan = '2'
+    }
+
+
+
+})
+
+const fr = document.createElement('form')
+document.body.appendChild(fr)
+
+const country = CreateFormFormat(fr,'nemzetiseg','Nemzetiség:')
+const author = CreateFormFormat(fr,'szerzo1','Szerző:')
+const title = CreateFormFormat(fr,'mu1','Mű:')
+const author2 = CreateFormFormat(fr,'szerzo2','Szerző:')
+const title2 = CreateFormFormat(fr,'mu2','Mű:')
+
+/**
+ * 
+ * @param {*} form 
+ * @param {string} id 
+ * @param {string} labelcontent 
+ */
+function CreateFormFormat(form,id,labelcontent){
+
+    const label = document.createElement('label')
+    label.innerText = labelcontent
+    label.htmlFor = id
+        
+    const input = document.createElement('input')
+    input.id = id
+
+
+    Break(form)
+    form.appendChild(label)
+    Break(form)
+    form.appendChild(input)
+    Break(form)
+
+
+}
+
+function Break(where){
+    
+    const br = document.createElement('br')
+    where.appendChild(br)
+
+}
+
+
+
+
+
+
