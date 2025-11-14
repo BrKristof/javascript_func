7022e3e879ae80fc7d1f6ec1600298f8667305d9
Brandhuber Kristof <brandhuber.kristof@BOLYAI.LOCAL>
2025-11-14 09:48:36 +0100

    refactor

---
+++ table_row/functions.js
@@ -0,0 +1,227 @@
+/**
+ * @typedef {{from:string ,person:string ,piece:string ,piece2:?string ,person2:?string}} CountryWriters
+ */
+
+/**
+ * 
+ * @param {*} form 
+ * @param {string} id 
+ * @param {string} labelcontent 
+ */
+function CreateFormFormat(form,id,labelcontent){
+
+    const label = document.createElement('label')
+    label.innerText = labelcontent
+    label.htmlFor = id
+        
+    const input = document.createElement('input')
+    input.id = id
+
+
+    Break(form)
+    form.appendChild(label)
+    Break(form)
+    form.appendChild(input)
+    Break(form)
+
+
+}
+
+/**
+ * 
+ * @param {*} where 
+ */
+function Break(where){
+    
+    const br = document.createElement('br')
+    where.appendChild(br)
+
+}
+
+/**
+ * 
+ * @param {string} text 
+ * @param {} where 
+ */
+function button(text,where){
+    
+    const button = document.createElement('button')
+    button.innerText = text
+    where.appendChild(button)
+
+}
+
+/**
+ * 
+ * @param {string[]} array 
+ */
+function renderTableBody(array){
+
+    const innerbody = document.getElementById('tbody')
+    innerbody.innerHTML = ""
+    
+
+    for (const a of array){
+
+        renderTableRow(innerbody,a) //itt hozzuk letre a sorokat, a for-on beluli valoto kerul a CountryWriters helyére és a cikluson belül lévő body a tbody helyére
+
+    }
+
+}
+
+/**
+ * 
+ * @param {HTMLTableElement} tbody 
+ * @param {*} CountryWriters 
+ */
+function renderTableRow(tbody,CountryWriters)// CountryWriters ami eltárolja az adatokak 
+{
+
+    const tr = document.createElement('tr')
+    tbody.appendChild(tr)
+
+    const td = createCell('td',CountryWriters.from,tr)
+
+    td.addEventListener("click",function(e){
+    /**
+     * @type {HTMLTableCellElement}
+     */
+    const i = e.target
+            
+    const row = i.parentElement
+    const tbody = row.parentElement
+
+    const marked = tbody.querySelector('.marked')
+
+    if(marked != Null){
+        marked.classList.remove(marked)
+    }
+    marked.classList.add()
+
+    i.classList.add('marked')
+
+    })
+
+    const td2 = createCell('td',CountryWriters.person,tr)
+    const td3 = createCell('td',CountryWriters.piece,tr)
+
+    if(CountryWriters.person2 != undefined && CountryWriters.piece2 != undefined){
+
+        const ptr = document.createElement('tr')
+        tbody.appendChild(ptr)
+
+        const td4 = createCell('td',CountryWriters.person2,ptr)
+        const td5 = createCell('td',CountryWriters.piece2,ptr)
+
+        td.rowSpan = '2'
+    }
+}
+
+/**
+ * 
+ * letrehoz egy cellát és visszatér vele
+ * 
+ * @param {"td" | "th"} celltype 
+ * @param {string} cellcontent 
+ * @param {HTMLTableRowElement} parentrow 
+ * 
+ * @returns {HTMLTableCellElement}
+ */
+function createCell(celltype,cellcontent,parentrow){
+
+    const cell = document.createElement(celltype)
+    cell.innerText = cellcontent
+    parentrow.appendChild(cell)
+
+    return cell
+}
+
+/**
+ * @param {HTMLTableElement} table 
+ * @param {string[]} header_list 
+ */
+function headCreate(table,header_list){
+    
+    const thead = document.createElement('thead')
+    table.appendChild(thead)
+
+    const tr = document.createElement('tr')
+    for(const a of header_list){
+
+        const th = createCell('th',a,tr)
+        tr.appendChild(th)
+    }
+    thead.appendChild(tr)
+    
+    table.appendChild(thead)
+
+}
+
+function htmlFormEventListener(e){
+
+    e.preventDefault()
+    /**
+     * @type {HTMLFormElement}
+     */
+    const form = e.target
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const n = form.querySelector('#nemzetiseg')
+    /**
+     * @type {string}
+     */
+    const nvalue = n.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const a = form.querySelector('#mu1')
+        /**
+     * @type {string}
+     */
+    const avalue = a.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const p = form.querySelector('#nemzetiseg')
+        /**
+     * @type {string}
+     */
+    const pvalue = p.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const a2 = form.querySelector('#szerzo2')
+        /**
+     * @type {string}
+     */
+    const a2value = a2.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const p2= form.querySelector('#mu2')
+        /**
+     * @type {string}
+     */
+    const p2value = p2.value
+
+
+    /**
+     * @type {CountryWriters}
+     */
+    const obj  = {}
+    obj.from  = nvalue
+    obj.a  = avalue
+    obj.p = pvalue
+    obj.a2 = a2value
+    obj.p2 = p2value
+
+    const tbody = document.getElementById('body')
+
+    renderTableRow(tbody,obj)
+}
--- table_row/kitero.js
+++ table_row/kitero.js
@@ -1,6 +1,5 @@
-
 /**
- * @type {{from:string ,person:string ,piece:string ,piece2:?string ,person2:?string}[]}
+ * @type {CountryWriters[]}
  */
 const arr = [
     {
@@ -37,21 +36,10 @@ const table = document.createElement('table')
 document.body.appendChild(table)
 
 
-const thead = document.createElement('thead')
-table.appendChild(thead)
+
 
 const headarr = ['Nemzetiség','Szerző','Mű']
-const htr = document.createElement('tr')
-thead.appendChild(htr)
-
-for(const a of headarr){
-
-    const th = document.createElement('th')
-    th.innerText = a
-    htr.appendChild(th)
-
-     
-}
+const th = headCreate(table,headarr)
 
 const tbody = document.createElement('tbody')
 table.appendChild(tbody)
@@ -59,104 +47,7 @@ tbody.id = 'tbody'
 
 
 const form = document.getElementById('htmlform')
-form.addEventListener('submit', function(e){
-
-    e.preventDefault()
-    /**
-     * @type {HTMLFormElement}
-     */
-    const form = e.target
-
-    /**
-     * @type {HTMLInputElement}
-     */
-    const n = form.querySelector('#nemzetiseg')
-    /**
-     * @type {string}
-     */
-    const nvalue = n.value
-
-    /**
-     * @type {HTMLInputElement}
-     */
-    const a = form.querySelector('#mu1')
-        /**
-     * @type {string}
-     */
-    const avalue = a.value
-
-    /**
-     * @type {HTMLInputElement}
-     */
-    const p = form.querySelector('#nemzetiseg')
-        /**
-     * @type {string}
-     */
-    const pvalue = p.value
-
-    /**
-     * @type {HTMLInputElement}
-     */
-    const a2 = form.querySelector('#szerzo2')
-        /**
-     * @type {string}
-     */
-    const a2value = a2.value
-
-    /**
-     * @type {HTMLInputElement}
-     */
-    const p2= form.querySelector('#mu2')
-        /**
-     * @type {string}
-     */
-    const p2value = p2.value
-
-
-    /**
-     * @type {{from:string ,person:string ,piece:string ,piece2:?string ,person2:?string}}
-     */
-    const obj  = {}
-    obj.from  = nvalue
-    obj.a  = avalue
-    obj.p = pvalue
-    obj.a2 = a2value
-    obj.p2 = p2value
-
-    const tbody = document.getElementById('body')
-
-    const tr = document.createElement('tr')
-    tbody.appendChild(tr)
-
-    const td = document.createElement('td')
-    td.innerText = nvalue
-    tr.appendChild(td)
-
-    const td2 = document.createElement('td')
-    td2.innerText = avalue
-    tr.appendChild(td2)
-
-    const td3 = document.createElement('td')
-    td3.innerText = pvalue
-    tr.appendChild(td3)
-
-    if(a2value != '' && p2value != ''){
-
-        const ptr = document.createElement('tr')
-        tbody.appendChild(ptr)
-
-        const td4 = document.createElement('td')
-        td4.innerText = a2value
-        ptr.appendChild(td4)
-
-        const td5 = document.createElement('td')
-        td5.innerText = p2value
-        ptr.appendChild(td5)
-
-        td.rowSpan = '2'
-    }
-
-})
+form.addEventListener('submit', htmlFormEventListener)
 
 
 const fr = document.createElement('form')
@@ -222,7 +113,7 @@ fr.addEventListener('submit', function(e){
 
 
     /**
-     * @type {{from:string ,person:string ,piece:string ,piece2:?string ,person2:?string}}
+     * @type {CountryWriters}
      */
     const obj  = {}
     obj.from = nvalue
@@ -237,118 +128,11 @@ fr.addEventListener('submit', function(e){
     renderTableBody(arr)
 })
 button('Hozzáadás',fr)
-
-
-/**
- * 
- * @param {string} form 
- * @param {string} id 
- * @param {string} labelcontent 
- */
-function CreateFormFormat(form,id,labelcontent){
-
-    const label = document.createElement('label')
-    label.innerText = labelcontent
-    label.htmlFor = id
-        
-    const input = document.createElement('input')
-    input.id = id
-
-
-    Break(form)
-    form.appendChild(label)
-    Break(form)
-    form.appendChild(input)
-    Break(form)
-
-
-}
-
-
-function Break(where){
-    
-    const br = document.createElement('br')
-    where.appendChild(br)
-
-}
-
-function button(text,where){
-    
-    const button = document.createElement('button')
-    button.innerText = text
-    where.appendChild(button)
-
-}
-
-
-
-function renderTableBody(array){
-
-    const innerbody = document.getElementById('tbody')
-    innerbody.innerHTML = ""
-    
-
-    for (const a of array){
-
-
-        const tr = document.createElement('tr')
-        innerbody.appendChild(tr)
-
-        const td = document.createElement('td')
-        td.innerText = a.from
-
-        td.addEventListener("click",function(e){
-            /**
-             * @type {HTMLTableCellElement}
-             */
-            const i = e.target
-            
-
-            const row = i.parentElement
-            const tbody = row.parentElement
-
-            const marked = tbody.querySelector('.marked')
-
-            if(marked != Null){
-                marked.classList.remove(marked)
-            }
-            marked.classList.add()
-
-            i.classList.add('marked')
-
-
-        })
-        tr.appendChild(td)
-
-        const td2 = document.createElement('td')
-        td2.innerText = a.person
-        tr.appendChild(td2)
-
-        const td3 = document.createElement('td')
-        td3.innerText = a.piece
-        tr.appendChild(td3)
-
-        if(a.person2 != undefined && a.piece2 != undefined){
-
-            const ptr = document.createElement('tr')
-            innerbody.appendChild(ptr)
-
-            const td4 = document.createElement('td')
-            td4.innerText = a.person2
-            ptr.appendChild(td4)
-
-            const td5 = document.createElement('td')
-            td5.innerText = a.piece2
-            ptr.appendChild(td5)
-
-            td.rowSpan = '2'
-        }
-
-
-    }
-
-}
 renderTableBody(arr)
+
+
+
+
 
 
 
